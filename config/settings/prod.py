8966d4f4c38678a81661e6ca5d7462bab5e0ffe1
sky81219 <=>
2023-04-28 17:21:53 +0900

    초기설정

---
+++ config/settings/prod.py
@@ -0,0 +1,11 @@
+from .common import *
+
+DEBUG = False
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
