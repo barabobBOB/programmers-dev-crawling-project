ab6a932b203589294b644390d5cd69d1759fad37
hyangsso <sohyang8946@gmail.com>
2023-05-02 16:04:34 +0900

    feat: features 카테고리 제외

--- backend/crawling/cointelegraph_crawling.py
+++ backend/crawling/cointelegraph_crawling.py
@@ -12,26 +12,39 @@ time.sleep(3)
 # 기사 관련 내용만 선택
 features = driver.find_elements(By.PARTIAL_LINK_TEXT, "Features")
 
-# news_urls = []
 titles = []
 contents = []
+dates = []
 
 for feature in features:
-    feature.send_keys(Keys.ENTER)
-    
-    # current_url = driver.current_url
-    # news_urls.append(current_url)
 
+    # 기사가 아닌 카테고리 features는 제외
+    if feature.text == 'Features':
+        print(feature.text)
+        continue
+    
+    time.sleep(4)
+    feature.send_keys(Keys.ENTER)
+
+    # 기사 제목
     current_title = driver.title
     titles.append(current_title)
 
+    '''
+    본문 내용 전체
     current_content = driver.find_element(By.CLASS_NAME, "article__inner.body-l").text
+    '''
+
+    # 기사 날짜
+    current_date = driver.find_element(By.XPATH, "/html/body/div/main/section[1]/div/div[1]/div[1]/div[2]/span[3]").text
+    dates.append(current_date)
+
+    # 기사 요약본 (부제목) 
+    current_content = driver.find_element(By.CLASS_NAME, "article-top__title__subtitle").text
     contents.append(current_content)
-
-    # 추가되는 페이지에 대해 코드 추가해야 함
-
+    
     driver.back()
 
-dfresult = pd.DataFrame(zip(titles, contents))
-dfresult.columns = ['title', 'content']
-
+dfresult = pd.DataFrame(zip(titles, dates, contents))
+dfresult.columns = ['title', 'date', 'content']
+print(dfresult)
