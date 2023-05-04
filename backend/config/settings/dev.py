3c219ad46f7231eae2da24edf02ecac708b5f2b2
sky81219 <=>
2023-05-04 19:02:26 +0900

    refactor: code 병합

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -16,12 +16,6 @@ CACHES = {
 
 REST_FRAMEWORK = {
     ###'NON_FIELD_ERRORS_KEY': 'error',###  요거 제외밑에부터       
-<<<<<<< HEAD
-=======
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    # ),
->>>>>>> feat/#4
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
