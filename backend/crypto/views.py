6471da90010d12c6efb0d313a5aca1e9f752a981
sky81219 <=>
2023-05-07 22:55:03 +0900

    refactor: coin_news-값-objects-성공

--- backend/crypto/views.py
+++ backend/crypto/views.py
@@ -4,7 +4,7 @@ from crawling.googlenews_crawling import *
 from .models import *
 
 # 최신 뉴스 10개 크롤링
-def recent_news():
+def recent_news() -> None:
     coinness_news = coinness_crawling()
     coinness_news.reverse()
 
@@ -15,22 +15,17 @@ def recent_news():
                                     time=news['time'])
 
 # 해당 코인 뉴스 크롤링 
-def coin_news():
-    coin_name = ['BTC', 'ETH', 'MTL', 'XRP', 'ETC', 'SNT', 'WAVES', 'QTUM', 'STEEM', 
-                 'XLM', 'REP', 'ADA', 'POWR', 'BTG', 'ICX', 'EOS', 'TRX', 'ONT',
-                 'ZIL', 'ZRX', 'LOOM', 'BCH', 'BAT', 'LOST', 'ONG', 'ELF', 'KNC', 
-                 'BSV', 'THETA', 'QKC', 'BTT', 'ENJ', 'TFUEL', 'MANA', 'ANKR',
-                 'AERGO', 'ATOM', 'MBL', 'WAXP', 'MED', 'MLK', 'STPT', 'ORBS', 'VET', 
-                 'CHZ', 'HIVE', 'LINK', 'XTZ', 'BORA', 'JST', 'CRO', 'SXP', 'PLA', 
-                 'DOT', 'STRAX', 'AQT', 'GLM', 'SSX', 'META', 'FCT2', 'SAND', 'DOGE', 
-                 'PUNDIX', "FLOW", "AXS", "XEC", "SOL", "MATIC", "AAVE", "1INCH"
-                 ,'ALGO', 'AVAX', 'T', 'GMT', 'APT', 'SHIB', 'ARB', 'EGLD', 'SUI']
-                 
+def coin_news() -> None:
+    coin_name = CoinSymbol.objects.values("coin_symbol")          
+    
     for i in coin_name:
-        google_coin_news = news_crawling(i)
+        google_coin_news = news_crawling(i["coin_symbol"])
 
         for news in google_coin_news:
             CrawlingInformation.objects.create(name=news['name'], 
                                                 titles=news['title'], 
                                                 dates=news['date'], 
                                                 urls=news['url'])
+            
+            
+            
